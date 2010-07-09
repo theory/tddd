672dead6857a59050a5fa6bb18c9f26b6346a302
David E. Wheeler <david@justatheory.com>
2010-07-08 18:44:15 -0700

    Add `upd_pass()`.

---
+++ sql/012-userfunc.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION ins_user(
+    nickname TEXT,
+    password TEXT
+) RETURNS VOID LANGUAGE SQL AS $$
+    INSERT INTO users values($1, crypt($2, gen_salt('md5')));
+$$;
+
+CREATE OR REPLACE FUNCTION upd_pass(
+    nick    TEXT,
+    oldpass TEXT,
+    newpass TEXT
+) RETURNS BOOL LANGUAGE plpgsql AS $$
+BEGIN
+    UPDATE users
+       SET password = crypt($3, gen_salt('md5'))
+     WHERE nickname = $1
+       AND password = crypt($2, password);
+    RETURN FOUND;
+END;
+$$;
