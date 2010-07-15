a53d7c0434ad97eed3fafb8f9ad04d88d773d01a
David E. Wheeler <david@justatheory.com>
2010-07-15 11:16:03 -0700

    Add tests for flyps and ignored tables. to deck.

--- sql/022-flypfunc.sql
+++ sql/022-flypfunc.sql
@@ -38,3 +38,55 @@ BEGIN
     END LOOP;
 END;
 $$;
+
+\q
+
+
+CREATE OR REPLACE FUNCTION del_flyp(
+   flyp_id TEXT
+) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER AS $$
+BEGIN
+    DELETE FROM flyps WHERE id = flyp_id;
+    RETURN FOUND;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION ins_ignore(
+    nick        TEXT,
+    ignore_nick TEXT
+) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER AS $$
+BEGIN
+    INSERT INTO ignored (user_nick, ignored_nick)
+    VALUES (nick, ignore_nick);
+    RETURN true;
+EXCEPTION WHEN unique_violation THEN
+    RETURN false;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION del_ignore(
+    nick        TEXT,
+    ignore_nick TEXT
+) RETURNS BOOLEAN LANGUAGE plpgsql SECURITY DEFINER AS $$
+BEGIN
+    DELETE FROM ignored
+     WHERE user_nick    = nick
+       AND ignored_nick = ignore_nick;
+    RETURN FOUND;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION get_flyps_for(
+    nickname TEXT,
+    offset   int,
+    limit    int,
+) RETURNS SETOF flyps LANGUAGE sql SECURITY DEFINER AS $$
+    SELECT *
+      FROM flyps
+      LEFT JOIN ignored ON flyps.nickname = ignored.ignored_nick
+     WHERE ignored.user_nick = $1
+       AND ignored.ignored_nick IS NULL
+     ORDER BY timestamp DESC
+    OFFSET COALESCE($2, 0),
+     LIMIT COALESCE($3, 25);
+$$;
